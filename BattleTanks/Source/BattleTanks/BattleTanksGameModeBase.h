e785061c44878de7fd7b800b46257ca9356ab46a
zDonik <tokhirovdoniyor@gmail.com>
2020-08-12 14:28:03 +0500

    Created project

---
+++ BattleTanks/Source/BattleTanks/BattleTanksGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattleTanksGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANKS_API ABattleTanksGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
